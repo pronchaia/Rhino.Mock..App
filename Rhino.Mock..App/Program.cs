55502c469db2115026e6bbdf4f1aaa2535884269
pronchaia <pronchai.a@outlook.com>
2018-12-26 17:23:16 +0700

    Init

---
+++ Rhino.Mock..App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rhino.Mock._.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
